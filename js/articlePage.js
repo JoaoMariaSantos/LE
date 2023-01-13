ad2f1e078f87ca472c8f8ebe8589c47c961ee670
João Santos <joaosantosnatal@gmail.com>
2023-01-13 02:43:50 +0000

    article almost done

--- js/articlePage.js
+++ js/articlePage.js
@@ -11,7 +11,7 @@ let scrollEffectElements = document.querySelectorAll('#article *:not(div)');
 const nElementsToIgnore = 2;
 
 //images
-const nImages = 25;
+const nImages = 500;
 const loadedCheck = new Array(nImages);
 
 //container
@@ -22,14 +22,18 @@ const horizontalMoveMagnitude = 0.3;
 const verticalMoveMagnitude = 0.05;
 
 //valores a multiplicar por largura/altura da janela utilizados no getTransform()
-const horizontalEffectMagnitude = 0.2;
-const verticalEffectMagnitude = 0.3;
+const horizontalEffectMagnitude = 0.07;
+const verticalEffectMagnitude = 0.08;
 
 //window size
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
 
+//getImages(nImages);
+
+let nLoaded = 0;
 getImages(nImages);
+addScrollAnimations(scrollEffectElements);
 
 function getImages(n) {
     const folder = container.classList[0];
@@ -37,20 +41,24 @@ function getImages(n) {
 
     for (let i = 0; i < n; i++) {
         const img = document.createElement('img');
-        img.style.marginLeft = 'auto';
-        img.style.marginTop = 'auto';
+        img.className = "article--effect";
+        if(Math.random()*1 < 0.3) img.classList.add("article--effect__colored")
+        img.style.position = "absolute";
+        img.style.left = Math.floor(- 100 + Math.random() * container.clientWidth + 100) + 'px';
+        img.style.top = Math.floor(Math.random() * container.scrollHeight) + 'px';
         img.src = "/assets/" + folder + "/img (" + (Math.floor(Math.random() * existingImages) + 1) + ").png";
-        container.insertBefore(img, scrollEffectElements[nElementsToIgnore + Math.floor(Math.random() * (scrollEffectElements.length - nElementsToIgnore))]);
+        container.append(img);
 
-        loadedCheck[i] = false;
+        //loadedCheck[i] = false;
         img.addEventListener('load', imgLoaded(i));
     }
 }
 
 function imgLoaded(i) {
-    loadedCheck[i] = true;
-
-    if (!loadedCheck.includes(undefined)) moveRandomly(document.querySelectorAll('#article *:not(div)'));
+    //loadedCheck[i] = true;
+    nLoaded ++;
+    console.log(nLoaded + ' / ' + nImages);
+    //if (!loadedCheck.includes(undefined)) addScrollAnimations(document.querySelectorAll('#article .article--effect'));
 }
 
 function moveRandomly(elements) { //mexe os elementos um bcd aleatoriamente
@@ -64,18 +72,18 @@ function moveRandomly(elements) { //mexe os elementos um bcd aleatoriamente
 }
 
 function addScrollAnimations(elements) {
-
-    console.log(elements);
-
     elements.forEach(elem => {
 
         const enterTransform = getTransform();
+        const enterFilter = getFilter();
 
         elem.animate( //animação entrada do elemento
             {
-                transform: enterTransform //transformações. dá com qq propriedade css
+                transform: enterTransform, //transformações. dá com qq propriedade css
+                //filter: enterFilter
             },
             {
+                easing: 'cubic-bezier(0, 0.55, 0.45, 1)',
                 duration: 1,
                 timeline: new ViewTimeline({
                     subject: elem,
@@ -86,31 +94,57 @@ function addScrollAnimations(elements) {
             })
 
         const exitTransform = enterTransform.reverse();
+        const exitFilter = enterFilter.reverse();
 
-        elem.animate( //animação saída do elemento
-            {
-                transform: exitTransform
-            },
-            {
-                duration: 1,
-                timeline: new ViewTimeline({
-                    subject: elem,
-                    axis: "vertical",
-                }),
-                delay: { phase: "exit", percent: CSS.percent(-100) }, //percentagem do elemento que saiu da div
-                endDelay: { phase: "exit", percent: CSS.percent(100) }
-            })
+        if (elem.id == 'title') {
+            elem.animate( //animação saída do elemento
+                {
+                    transform: exitTransform,
+                    filter: exitFilter
+                },
+                {
+                    easing: 'cubic-bezier(0, 0.55, 0.45, 1)',
+                    duration: 1,
+                    timeline: new ViewTimeline({
+                        subject: elem,
+                        axis: "vertical",
+                    }),
+                    delay: { phase: "exit", percent: CSS.percent(0) }, //percentagem do elemento que saiu da div
+                    endDelay: { phase: "exit", percent: CSS.percent(200) }
+                })
+        } else {
+
+            elem.animate( //animação saída do elemento
+                {
+                    transform: exitTransform,
+                    filter: exitFilter
+                },
+                {
+                    easing: 'cubic-bezier(0, 0.55, 0.45, 1)',
+                    duration: 1,
+                    timeline: new ViewTimeline({
+                        subject: elem,
+                        axis: "vertical",
+                    }),
+                    delay: { phase: "exit", percent: CSS.percent(0) }, //percentagem do elemento que saiu da div
+                    endDelay: { phase: "exit", percent: CSS.percent(300) }
+                })
+        }
     })
 
 }
 
 function getTransform() { //gera valores para o transform
     const translatedWidth = Math.floor(-windowWidth * horizontalEffectMagnitude + Math.random() * (windowWidth * horizontalEffectMagnitude * 2));
-    //const translatedWidth = 0;
 
-    const translatedHeight = Math.floor(-windowHeight * verticalEffectMagnitude + Math.random() * (windowHeight * verticalEffectMagnitude * 2));
-    //const translatedHeight = windowHeight;
+    //const translatedHeight = Math.floor(-windowHeight * verticalEffectMagnitude + Math.random() * (windowHeight * verticalEffectMagnitude * 2));
+    const translatedHeight = -Math.floor(Math.random() * (windowHeight * verticalEffectMagnitude * 2));
 
-    return ["translate(" + translatedWidth + "px, " + translatedHeight + "px) scale(0.95)",
+    return ["translate(" + translatedWidth + "px, " + translatedHeight + "px) scale(0.8)",
         "translate(0) scale(1)"];
+}
+
+function getFilter() {
+    return ["blur(0.5px)",
+        "blur(0)"];
 }
