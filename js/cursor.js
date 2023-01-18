5222a906e7bb3fd0d8ac0f4f35c470b0fbddef79
jppgomez <77557946+jppgomez@users.noreply.github.com>
2023-01-18 22:38:39 +0000

    FIX

--- js/cursor.js
+++
@@ -1,9 +0,0 @@
-$(document).mousemove(function(e){
-    //Get 'container' offset:
-    o = $('.cursor').offset();
-    //Track mouse position:
-    $(".dot").css({
-      "top": e.pageY - o.top,
-      "left": e.pageX - o.left
-    });
-});
