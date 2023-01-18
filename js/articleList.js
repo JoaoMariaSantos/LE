6ae45c0d1a86eebb299950e36b71ccd9997bd310
João Santos <joaosantosnatal@gmail.com>
2023-01-18 19:12:12 +0000

    articleList scroll fix

--- js/articleList.js
+++ js/articleList.js
@@ -1,20 +1,30 @@
 const main = document.querySelector('#postList');
 const posts = document.querySelectorAll('.post');
 let currentPost = 0;
+let scrollEnabled = true;
 
-let backgroundScrollValue = 0;
+let backgroundScrollValue = Math.floor(Math.random() * 2000);
 const backgroundScrollInc = 1000;
 
 main.addEventListener('wheel', handleScroll);
 
-function handleScroll(){
-    currentPost ++;
-    if(currentPost >= posts.length) currentPost = 0;
+function handleScroll() {
+    if (scrollEnabled) {
+        currentPost++;
+        if (currentPost >= posts.length) currentPost = 0;
 
-    posts[currentPost].scrollIntoView({
-        behavior: 'smooth'
-    });
+        posts[currentPost].scrollIntoView({
+            behavior: 'smooth'
+        });
 
-    backgroundScrollValue += Math.floor(Math.random()*backgroundScrollInc);
-    main.style.backgroundPositionY = backgroundScrollValue + 'px';
+        backgroundScrollValue += Math.floor(Math.random() * backgroundScrollInc);
+        main.style.backgroundPositionY = backgroundScrollValue + 'px';
+
+        scrollEnabled = false;
+        setTimeout(function() { enableScroll(); }, 1000);
+    }
+}
+
+function enableScroll(){
+    scrollEnabled = true;
 }
